001fca0e741feca907d32346a8b63392821d9bd0
Jan Tvrdik <jan.tvrdik@gmail.com>
2013-08-11 16:51:34 +0200

    add nette micro

---
+++ micro/nette/index.php
@@ -0,0 +1,19 @@
+<?php
+use Nette\Configurator;
+use Nette\Application\Routers\Route;
+
+$configurator = new Configurator();
+$configurator->setDebugMode(FALSE);
+$configurator->setTempDirectory(__DIR__ . '/temp');
+
+$container = $configurator->createContainer();
+
+$container->router[] = new Route('say/hello/<name>', function ($name) {
+	return new Nette\Application\Responses\TextResponse('Hello ' . htmlspecialchars($name) . '!');
+	/*return [
+		'Hello {$name}!',
+		['name' => $name],
+	];*/
+});
+
+$container->application->run();
